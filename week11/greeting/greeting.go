9d109a9542a0293d67fbbc108a143cd5456c3b68
pakr geonhong <qkrrjsghd10@naver.com>
2024-11-08 11:33:31 +0900

    V3.3 Pakge

---
+++ week11/greeting/greeting.go
@@ -0,0 +1,10 @@
+package greeting
+
+import "fmt"
+
+func Hi(name string) {
+	fmt.Printf("Hi %s!\n", name)
+}
+func Hello(name string) {
+	fmt.Printf("Hi %s!\n", name)
+}
---
+++ week11/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "week11/greeting"
+
+func main() {
+	greeting.Hi("Inha")
+	greeting.Hello("Harvard")
+}
