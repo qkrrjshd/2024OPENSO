56c7e1fab78313bf3e38f61b2356d181b33bed53
pakr geonhong <qkrrjsghd10@naver.com>
2024-11-15 11:09:35 +0900

    v3.7 arrzy

--- week12/main.go
+++ week12/main.go
@@ -6,9 +6,12 @@ import (
 )
 
 func main() {
-	//var dates [3]time.Time
-	//dates[1] = time.Unix(1447920000, 0)
-	//fmt.Println(dates)
 	var dates = [3]time.Time{time.Unix(1, 0), time.Unix(2, 0)}
-	fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Println(dates)
+	//fmt.Printf("%#v\n", dates)
+
+	for i := 0; i < len(dates); i++ {
+		fmt.Println(i, dates[i])
+	}
 }
